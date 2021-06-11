8b03dc9793e56cd9d4da8caa0c5e0f86641c67d3
george <datameshprojects@gmail.com>
2021-06-11 14:32:35 +0100

    updated Dockerfile for windows 10 deployment

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, json
+import logging
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    app.logger.info("Main request successful")
     return "Hello World!"
 
+@app.route('/status')
+def status():
+    response = app.response_class(
+            response=json.dumps({"result":"OK - healthy"}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info("Status request successful")
+    return response
+
+@app.route('/metrics')
+def metrics():
+    response = app.response_class(
+            response=json.dumps({"status":"success","code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+            status=200,
+            mimetype='application/json'
+    )
+    app.logger.info("Metrics request successful")
+    return response
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    # stream logs to a file
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    app.run(host='0.0.0.0', debug=True)
